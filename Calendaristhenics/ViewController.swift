a2808da8da50ba6b13bf5050b86e6b6bc3105d28
Michael Gubik <gubikmic@gmail.com>
2018-08-08 17:00:18 +0200

    Initial Commit

---
+++ Calendaristhenics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calendaristhenics
+//
+//  Created by Michael Gubik on 2018-08-08.
+//  Copyright © 2018 Michael Gubik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
