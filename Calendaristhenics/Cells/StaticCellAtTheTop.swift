d206442a8fd67f40df7f15999184899a4b0facb9
Michael Gubik <gubikmic@gmail.com>
2018-08-09 15:42:36 +0200

    Enable removeAll and createAll

---
+++ Calendaristhenics/Cells/StaticCellAtTheTop.swift
@@ -0,0 +1,13 @@
+//
+//  StaticCellAtTheTop.swift
+//  Calendaristhenics
+//
+//  Created by Michael Gubik on 2018-08-09.
+//  Copyright © 2018 Michael Gubik. All rights reserved.
+//
+
+import UIKit
+
+class StaticCellAtTheTop: UITableViewCell {
+    @IBOutlet weak var button: UIButton!
+}
--- Calendaristhenics/Cells/WorkoutCell.swift
+++ Calendaristhenics/Cells/WorkoutCell.swift
@@ -13,16 +13,4 @@ class WorkoutCell: UITableViewCell {
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var button: UIButton!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
